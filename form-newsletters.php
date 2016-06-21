a7b83b86a0a7d09a13e6f68e63290005244821ba
fabulouse <birba.fabien@gmail.com>
2016-06-21 14:38:27 +0200

    ignore

--- form-newsletters.php
+++ form-newsletters.php
@@ -1,4 +1,5 @@
 <form method="post" action="process.php">
+<<<<<<< Updated upstream
     <input id="nom" name="name" type="text" placeholder="Nom" required>
     <input id="prenom" name="firstname" value="" type="text" placeholder="Prénom">
     <input value="" name="email" type="email" placeholder="E-mail" required>
@@ -16,6 +17,19 @@
             <label for="both_profile" title="information du client">Les deux</label>
             <input type="radio" name="user-type" value="Both" id="both_profile">
 
+=======
+    <input id="nom" name="name"  value="" type="text" placeholder="Nom" required>
+    <input id="prenom" name="firstname" value="" type="text" placeholder="Prénom" required>
+    <input id="mail-news" name="email" value="" type="email" placeholder="E-mail" required>
+    <fieldset>
+        <legend>Rejoindre la communauté :</legend>
+            <label for="loueur_profile" title="type de profile du client">Loueur</label>
+            <input type="radio" name="user-type" value="Loueur" id="loueur_profile" >
+            <label for="locataire_profile" title="information du client">Locataire</label>
+            <input type="radio" name="user-type" value="Locataire" id="locataire_profile" >
+            <label for="both_profile" title="information du client">Les deux</label>
+            <input type="radio" name="user-type" value="Both" id="both_profile">
+>>>>>>> Stashed changes
     </fieldset>
     <button id="newsletter-submit" name="news_send" value="1" type="submit">S'INSCRIRE</button>
 </form>
